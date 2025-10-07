0581507de2b9a3b6d410c3818a5b8fe53dee85c4
Eero H <eero.h@live.fi>
2025-10-07 10:44:17 +0300

    nappulo

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,15 @@
+function Nappulo() {
+  return (
+    <button onclick="Alert('moi')">nappulo</button>
+  )
+}
+
 export default function Home() {
   return (
     <main>
       <div>Next.js on GitHub Pages</div>
       <div>glup</div>
-      <button onclick="Alert('moi')>nappulo</button>
+      <Nappulo/>
     </main>
   );
 }
